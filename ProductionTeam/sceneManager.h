c5192e77e9445e612918569dc0b60417e8ab1e53
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-09-21 16:09:29 +0900

    コメント文追加

--- ProductionTeam/sceneManager.h
+++ ProductionTeam/sceneManager.h
@@ -35,6 +35,10 @@ public:
 
 	void TitleDraw();
 
+	/// <summary>
+	/// ここから説明画面用
+	/// </summary>
+
 	void ExplanationUpdate();
 
 	void ExplanationDraw();
@@ -53,6 +57,8 @@ public:
 
 	void ClearUpdate();
 
+public:
+
 	GAMESCENE GetScene() { return gameScene; }
 
 	bool GetGameEnd() { return gameEnd; }
@@ -61,12 +67,19 @@ private:
 	char keys[356] = {};
 	char preKeys[356] = {};
 
+	//ゲームパッドのスティック用
 	int leftX = 0;
 	int leftY = 0;
 
+	//エスケープをオスとゲーム終了できるようにするためのフラグ
 	bool gameEnd = false;
 
+	//スティック選択時に過度に動きすぎないようにする
 	int count1 = 0;
+
+	/// <summary>
+	/// ここから音楽と画像
+	/// </summary>
 
 	int arrowImage = Novice::LoadTexture("./NoviceResources/arrow.png");
 	int EXImage = Novice::LoadTexture("./NoviceResources/EXban.png");
@@ -80,6 +93,10 @@ private:
 	int gameVoice = -1;
 	int clearMusic = Novice::LoadAudio("./NoviceResources/fanfare.wav");
 	int clearVoice = -1;
+
+	/// <summary>
+	/// ここまで
+	/// </summary>
 
 	Player* player = new Player;
 	Map* m = new Map;
